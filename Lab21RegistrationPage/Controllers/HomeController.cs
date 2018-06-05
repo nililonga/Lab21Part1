940b7b6accda5e74aaed20eb25e0bbf13028c9e9
noelle none <noe.ililonga@gmail.com>
2018-06-05 08:01:53 -0400

    lab24b update

--- Lab21RegistrationPage/Controllers/HomeController.cs
+++ Lab21RegistrationPage/Controllers/HomeController.cs
@@ -54,6 +54,38 @@ namespace Lab21RegistrationPage.Controllers
 
                             return View();
         }
-       
+        public ActionResult Menu(Item data)
+        {
+            CoffeeShopDBEntities ORM = new CoffeeShopDBEntities();
+            List<Item> items = ORM.Items.ToList();
+            ViewBag.Items = items;
+            ViewBag.Items = ORM.Items;
+            return View();
+        }
+        public ActionResult Add(int id)
+        {
+            CoffeeShopDBEntities ORM = new CoffeeShopDBEntities();
+            //check if cart object already exists
+            if (Session["Cart"] == null)
+            {
+                //if it doesn't, make new list of books
+                List<Item> cart = new List<Item>();
+                //add this book to it
+                cart.Add((from x in ORM.Items where x.ID == id select x).Single());
+                //add the list to the session
+                Session.Add("Cart", cart);
+            }
+            else
+            {
+                //if it does exist, get the list
+                List<Item> cart = (List<Item>)(Session["Cart"]);
+                //add this book to it
+                cart.Add((from x in ORM.Items where x.ID == id select x).Single());
+                //add it back to the session
+                //Session["Cart"] = cart;
+            }
+            return View();
+        }
+
     }
 }
